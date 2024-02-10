9f85e006cef0d3b50e13b3a8e052641cabc2acad
deepakkolapati <deepakchandu9@gmail.com>
2024-02-10 10:37:51 +0530

    [deepak] Added login and register routes

---
+++ routes/user.py
@@ -0,0 +1,37 @@
+from core import db, init_app
+from flask import request
+from core.models import User
+from schemas.user_schemas import UserSchema, UsernameValidator
+from pydantic import ValidationError
+import json
+
+app = init_app()
+
+@app.route('/')
+def index():
+    return {'message': 'Fundoo Notes'}
+
+@app.post("/register")
+def create_user():
+    try:
+        serializer = UserSchema(**request.json)
+        user=User(**serializer.model_dump())
+        db.session.add(user)
+        db.session.commit()
+        return {"message": "user registered", "status" : 201, "data" : user.json}
+    except ValidationError as e:
+        err = json.loads(e.json())
+        return {'message': f'{err[0]['input']}-{err[0]['msg']}', "status": 400}, 400
+    
+@app.post("/login")
+def login_user():
+    data = request.get_json()
+    try:
+        serializer = UsernameValidator(username=data['username'])
+        user=User.query.filter_by(username=data['username']).first()
+        if not user and user.password==data['password']:
+            return {"message" : "Username or Password is incorrect"}, 401
+        return {"message":" User logged in successfully"}, 200
+    except ValidationError as e:
+        return {'message': 'Invalid username'}, 400
+
