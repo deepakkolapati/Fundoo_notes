2c8f788bb020109603e103b98f6bc61cc1d9be4e
deepakkolapati <deepakchandu9@gmail.com>
2024-02-11 19:17:35 +0530

    [deepak] Added flask mail

--- core/__init__.py
+++ core/__init__.py
@@ -1,15 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
 
 db = SQLAlchemy()
 migarte = Migrate()
+mail = Mail()
 
 def init_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:9033@localhost:5432/fundoo_notes'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USERNAME'] = 'deepakchandu9@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'nkng rovm okmq qidd'
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
+    mail.init_app(app)
     db.init_app(app)
     migarte.init_app(app, db)
 
