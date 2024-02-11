42bfd911c2a0a3c14ee3654534bdb8c1ef8b3ec3
deepakkolapati <deepakchandu9@gmail.com>
2024-02-11 19:19:33 +0530

    [deepak] Added sending mail function for token verification

--- routes/user.py
+++ routes/user.py
@@ -1,17 +1,20 @@
-from core import db, init_app
+from core import db, init_app,mail
 from flask import request,jsonify
 from core.models import User
 from core.utils import JWT
 from schemas.user_schemas import UserSchema, UsernameValidator
 from pydantic import ValidationError
+from flask_mail import Message
 import json
 
 
 app = init_app()
 
+
 @app.route('/')
 def index():
     return {'message': 'Fundoo Notes'}
+    
 
 @app.post("/register")
 def create_user():
@@ -20,7 +23,24 @@ def create_user():
         user=User(**serializer.model_dump())
         db.session.add(user)
         db.session.commit()
-        return {"message": "user registered",'token': user.token('register', 60), "status" : 201, "data" : user.json}
+        token=user.token('register',60)
+        receivers=[user.email]
+        msg=Message("Welcome to Fundoo_Notes! Verify Your Email to Get Started",sender="deepakchandu9@gmail.com",recipients=receivers)
+        msg.body=f'''
+ Dear {user.username},
+
+Welcome to Fundoo_Notes! We're thrilled to have you as part of our community. To get started, please verify your email address by entering the following verification token within the website:
+
+Verification Token: {token}
+
+This verification step ensures the security of your account and helps us keep our community safe. If you didn't create an account with Fundoo_Notes, please ignore this email.
+
+Thank you for choosing Fundoo_Notes! If you have any questions or need assistance, feel free to reach out to our support team at deepakchandu9@gmail.com.
+
+Best regards,
+Fundoo_Notes Team'''
+        mail.send(msg)
+        return {"message": "user registered", "status" : 201, "data" : user.json}
     except ValidationError as e:
         err = json.loads(e.json())
         return {'message': f'{err[0]['input']}-{err[0]['msg']}', "status": 400}, 400
