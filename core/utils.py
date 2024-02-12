2aa11f351c1a5e38c04446908bf1f5c605d59ebb
deepakkolapati <deepakchandu9@gmail.com>
2024-02-12 11:53:29 +0530

    [deepak] Added send mail function

--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,7 @@
 import jwt
+from flask_mail import Message
+from . import mail
+from settings import settings
 
 class JWT:
 
@@ -15,3 +18,21 @@ class JWT:
         decoded= jwt.decode(encoded, cls.key, algorithms=[cls.algorithm], audience=aud)
         return decoded
     
+
+def send_mail(user, email, token):
+    msg=Message("Welcome to Fundoo_Notes! Verify Your Email to Get Started",
+                sender=f"{settings.sender}",recipients=[email])
+    msg.body=f'''
+Dear {user},
+
+Welcome to Fundoo_Notes! We're thrilled to have you as part of our community. To get started, please verify your email address by entering the following verification token within the website:
+
+Verification Link: {f'{settings.base_url}/verify?token={token}'}
+
+This verification step ensures the security of your account and helps us keep our community safe. If you didn't create an account with Fundoo_Notes, please ignore this email.
+
+Thank you for choosing Fundoo_Notes! If you have any questions or need assistance, feel free to reach out to our support team at deepakchandu9@gmail.com.
+
+Best regards,
+Fundoo_Notes Team'''
+    mail.send(msg)
