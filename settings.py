e3022283c4e498848abfd9ebb8a51fe1fa917b88
deepakkolapati <deepakchandu9@gmail.com>
2024-02-12 11:52:35 +0530

    [deepak] Added settings file

---
+++ settings.py
@@ -0,0 +1,14 @@
+from pydantic_settings import SettingsConfigDict, BaseSettings
+from pydantic import PostgresDsn
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env', extra='ignore')
+
+    sender: str
+    mail_password: str
+    database_uri: str
+    mail_port: int
+    base_url: str
+
+
+settings = Settings()
