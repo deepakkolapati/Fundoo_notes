2fe4808014a58761c67d75cc8dcf9c9b2d115aae
deepakkolapati <deepakchandu9@gmail.com>
2024-02-13 16:40:19 +0530

    [deepak] Refactored message body

--- core/utils.py
+++ core/utils.py
@@ -22,17 +22,10 @@ class JWT:
 def send_mail(user, email, token):
     msg=Message("Welcome to Fundoo_Notes! Verify Your Email to Get Started",
                 sender=f"{settings.sender}",recipients=[email])
-    msg.body=f'''
-Dear {user},
-
-Welcome to Fundoo_Notes! We're thrilled to have you as part of our community. To get started, please verify your email address by entering the following verification token within the website:
-
-Verification Link: {f'{settings.base_url}/verify?token={token}'}
-
-This verification step ensures the security of your account and helps us keep our community safe. If you didn't create an account with Fundoo_Notes, please ignore this email.
-
-Thank you for choosing Fundoo_Notes! If you have any questions or need assistance, feel free to reach out to our support team at {settings.sender}.
-
-Best regards,
-Fundoo_Notes Team'''
+    
+    with open('message.txt') as file:
+        content = file.read()
+        content = content.format(user=user, base_url=settings.base_url, token=token, sender=settings.sender)
+        msg.body= content
     mail.send(msg)
+
