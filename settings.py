51f7facb8fd2f4886deb08994ed28c982ef0fc15
deepakkolapati <deepakchandu9@gmail.com>
2024-02-24 16:24:46 +0530

    seetings

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 from pydantic_settings import SettingsConfigDict, BaseSettings
 from pydantic import PostgresDsn
-
+from logging.config import dictConfig
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', extra='ignore')
 
@@ -16,3 +16,35 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s]: %(filename)s: %(levelname)s: %(lineno)d: %(message)s",
+                "datefmt": "%m/%d/%Y %I:%M:%S %p",
+                "style": "%",
+            },
+            "console": {"format": "%(message)s"},
+        },
+        "handlers": {
+            "console": {
+                "level": "INFO",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "console",
+            },
+            "file": {
+                "level": "WARNING",
+                "class": "logging.FileHandler",
+                "filename": "fundoo.log",
+                "formatter": "default",
+            },
+        },
+        # "root": {"level": "INFO", "handlers": ["console"]},
+        "loggers": {
+            "": {"level": "DEBUG", "handlers": ["file", "console"], "propagate": True}
+        },
+    }
+)
