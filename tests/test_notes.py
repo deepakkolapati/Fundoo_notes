77c7e54907d46ca9abe7bc354b34ee914d00f452
deepakkolapati <deepakchandu9@gmail.com>
2024-02-28 15:14:50 +0530

    [deepak] Added archive test case

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -154,3 +154,31 @@ def test_delete_note_should_return_success(user_client):
     delete_response = user_client.delete('/api/notes/2', headers={"Content-Type": "application/json","Authorization": token})
     assert delete_response.status_code == 204
     
+def test_archive_note_should_return_success(user_client):
+    register_data = {
+        "username": "Karan",
+        "email": "joshikfelix22@gmail.com",
+        "password": "Kc5656$ef",
+        "location": "srm"
+    }
+    user_client.post('/api/user', json=register_data, headers={"Content-Type": "application/json"})
+    login_data = {
+        "username": "Karan",
+        "password": "Kc5656$ef"
+    }
+    login_response = user_client.post('/api/login', json=login_data, headers={"Content-Type": "application/json"})
+    token=login_response.json['token']
+    assert login_response.status_code == 200  # OK
+    note_data={
+        "title": "top movies",
+        "description": "inception",
+        "color":"yellow"
+    }
+    note_response = user_client.post('/api/notes', json=note_data, headers={"Content-Type": "application/json",
+    "Authorization": token})
+    assert note_response.status_code == 201 # OK
+    archive_data={"id" : 1}
+    archive_response=user_client.patch('/api/archive',json=archive_data, headers={"Content-Type": "application/json",
+    "Authorization": token})
+    assert archive_response.status_code == 200
+    assert archive_response.json['message'] == 'Note is archived'
