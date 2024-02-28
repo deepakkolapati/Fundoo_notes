a19626caa7099f4de29177a2b3f881772caa9ac7
deepakkolapati <deepakchandu9@gmail.com>
2024-02-28 14:44:30 +0530

    [deepak] Refactored typo

--- conftest.py
+++ conftest.py
@@ -1,24 +1,25 @@
 import pytest
-from core import init_app, db
+from core import init_app,db
 from flask_restx import Api
-from routes import user
+from routes import user,notes
 from pathlib import Path
 import os
 
 
 @pytest.fixture
 def user_app():
-    path = Path(__file__).resolve().parent
-    print(path)
-    app = init_app()
-    db_path=os.path.join(path, 'test.sqlite3')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
-    app.config['TESTING'] = True
+   
+    app = init_app(mode="testing")
     with app.app_context():
         db.create_all()
     api = Api(app)
     api.add_resource(user.UserAPI, '/api/user')
     api.add_resource(user.LoginAPI, '/api/login')
+    api.add_resource(notes.NotesApi, '/api/notes')
+    api.add_resource(notes.NoteApi, '/api/notes/<int:id>')
+    api.add_resource(notes.ArchiveApi, '/api/archive')
+    api.add_resource(notes.TrashApi, '/api/trash')
+    api.add_resource(notes.CollaborateApi,'/api/collaborate')
     yield app
     with app.app_context():
         db.drop_all()
