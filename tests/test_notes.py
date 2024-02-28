37d0eb62d44800fba9d1f95f830b4928b2d2fb46
deepakkolapati <deepakchandu9@gmail.com>
2024-02-28 16:51:28 +0530

    [deepak] Removed unnecessary lines

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -49,6 +49,7 @@ def test_get_notes_should_return_success(user_client):
     assert note_response.status_code == 201 
     notes_get_response = user_client.get('/api/notes', headers={"Content-Type": "application/json","Authorization": token})
     assert notes_get_response.status_code == 200
+    print(notes_get_response.json)
     assert notes_get_response.json['message'] == 'Notes Found'
 
 def test_update_note_should_return_suceess(user_client):
@@ -301,7 +302,7 @@ def test_archive_note_not_found_should_return_success(user_client):
     assert get_archive_notes_response.status_code == 404
     assert get_archive_notes_response.json['message'] == 'Notes not found'
 
-################################################################
+
 
 def test_trash_note_should_return_success(user_client):
     register_data = {
@@ -365,7 +366,7 @@ def test_restore_trash_note_should_return_success(user_client):
     assert restore_trash_response.status_code == 200
     assert restore_trash_response.json['message'] == 'Note is restored'
 
-#########
+
 
 def test_get_trash_notes_should_return_success(user_client):
     register_data = {
